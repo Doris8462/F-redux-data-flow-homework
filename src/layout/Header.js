597605232b75537efd6f902ad4c15b5f2ca994cc
Doris8462 <liandou123>
2020-09-23 18:51:43 +0800

    [huangyunjie] finish header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import './Header.scss';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class Header extends Component {
   render() {
     return (
       <header className="header">
         <div className="header-wrapper">
-          <img src="" alt="头像" />
-          <span className="username">用户名</span>
-
-          <a className="sign">Sign out</a>
+          {this.props.avatar && <img src={this.props.avatar} alt="头像" />}
+          <span className="username">{this.props.name}</span>
+          <a
+            className="sign"
+            onClick={this.props.isLogin ? this.props.onSignOut : this.props.onSignIn}
+          >
+            {this.props.isLogin ? 'Sign Out' : 'Sign In'}
+          </a>
         </div>
       </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ profile, isLogin }) => {
+  return {
+    ...profile,
+    isLogin
+  };
+};
+
+const getUserData = () => {
+  return fetch('https://my-json-server.typicode.com/kevindongzg/demo/login', {
+    method: 'GET'
+  }).then(response => response.json());
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSignIn() {
+    getUserData().then(data => {
+      dispatch(signIn(data));
+    });
+  },
+  onSignOut() {
+    dispatch(signOut);
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
-import { combineReducers } from 'redux';
-import name from './sign';
-
-const reducers = combineReducers({
-  name
-});
-
-export default reducers;
+const sign = (
+  state = {
+    profile: undefined,
+    isLogin: false
+  },
+  action
+) => {
+  if (action.type === 'SIGN_IN') {
+    return { ...state, profile: action.payload, isLogin: true };
+  }
+  return { ...state, profile: undefined, isLogin: false };
+};
+export default sign;
